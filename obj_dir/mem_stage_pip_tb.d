ab01b36409e4aaf7fedb25393e9225737bb13eb5
GauthamMK <gauthamaanmk17@gmail.com>
2025-07-15 19:53:15 +0530

    Delete obj_dir directory

--- obj_dir/mem_stage_pip_tb.d
+++
@@ -1,7 +0,0 @@
-mem_stage_pip_tb.o: ../sim/mem_stage_pip_tb.cpp Vmem_stage_pip.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h \
- /usr/local/share/verilator/include/vltstd/svdpi.h
