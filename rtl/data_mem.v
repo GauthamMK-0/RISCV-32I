12b9a995612bfc62b005a45f4f84c2a319ed6563
GauthamMK <gauthamaanmk17@gmail.com>
2025-07-17 15:44:57 +0530

    Update data_mem.v

--- rtl/data_mem.v
+++ rtl/data_mem.v
@@ -1,5 +1,7 @@
-module data_mem (
+// Description: Synchronous data memory 
+// Supports byte-wise write enbables
 
+module data_mem (
     input wire clk,
     input wire [31:0] addr,
     input wire [31:0] write_data,
@@ -10,11 +12,11 @@ module data_mem (
 
 );
 
-reg [31:0] mem [0:1023];
+    reg [31:0] mem [0:1023];  // 1024 x 4 = 4 KB memory
 
 always @(posedge clk) begin
-    if (mem_write) begin
-        if (byte_enable[0]) mem[addr[11:2]][7:0] <= write_data[7:0];
+    if (mem_write) begin                                                   // word-alligned address
+        if (byte_enable[0]) mem[addr[11:2]][7:0] <= write_data[7:0];       // byte masking to write data
         if (byte_enable[1]) mem[addr[11:2]][15:8] <= write_data[15:8];
         if (byte_enable[2]) mem[addr[11:2]][23:16] <= write_data[23:16];
         if (byte_enable[3]) mem[addr[11:2]][31:24] <= write_data[31:24];
